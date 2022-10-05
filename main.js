ac841e688bfdeffbfa11d20ef1e5f139412e3d55
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2022-10-04 21:16:00 -0300

    Create GET using Axios

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const url = "http://localhost:5500/api"
 
 function getUsers() {
-  
+  axios.get(url)
+    .then(response => {
+      apiResult.textContent = JSON.stringify(response.data)
+    })
+    .catch(e => console.error(e))
 }
 
-getUsers
+getUsers()
